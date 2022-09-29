a1fdde2352d85e60aca3d7ad9215da1d5bb3d35d
Jan Trummer <jan.trummer@hotmail.com>
2022-09-29 16:36:53 +0200

    add reclass file

--- Sauerbraten/reversedClasses.h
+++ Sauerbraten/reversedClasses.h
@@ -4,11 +4,19 @@ struct Vector3 {
 	float x, y, z;
 };
 
+struct Vector2 {
+	float x, y;
+};
+
 class LocalPlayer
 {
 public:
 	Vector3 location; //0x0000
-	char pad_000C[352]; //0x000C
-	int32_t health; //0x016C
-	char pad_0170[711]; //0x0170
-}; //Size: 0x0437
+	char pad_000C[48]; //0x000C
+	Vector2 viewAngle; //0x003C
+	char pad_0044[316]; //0x0044
+	int32_t health; //0x0180
+	char pad_0184[44]; //0x0184
+	int32_t pistolAmmo; //0x01B0
+	char pad_01B4[660]; //0x01B4
+}; //Size: 0x0448
